98a8d8b39669c761399703594d6c0eb6cf6615e8
kumarsantos <Ssah5803@gmail.com>
2024-01-05 11:02:14 +0530

    footer corrected

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,12 @@
-import React from 'react';
-import Layout from './Layout';
-import Link from 'next/link';
+import React from "react";
+import Layout from "./Layout";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light border-light'>
-      <Layout className={`py-8 flex items-center justify-between`}>
+    <footer className="w-full border-t-1 border-solid border-dark/50 font-medium text-lg dark:text-light/50 dark:border dark:border-light/10">
+      <Layout className={`py-8 flex items-center justify-center`}>
         <span>{new Date().getFullYear()} &copy; All Rights reserved.</span>
-        <div className='flex items-center'>
-          Build with{' '}
-          <span className='text-primary text-2xl px-1 '>&#9825; </span>
-          by&nbsp;
-          <Link
-            href={'https://github.com/kumarsantos'}
-            className='underline underline-offset-2'
-            target='_blank'
-          >
-            CodeBucks
-          </Link>
-        </div>
-        <Link
-          className='underline underline-offset-2'
-          href={'https://github.com/kumarsantos'}
-          target='_blank'
-        >
-          Say hello
-        </Link>
       </Layout>
     </footer>
   );
